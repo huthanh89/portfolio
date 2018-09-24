042bb4d9e3c59d455a76569ab384df9fc13a5ce0
Thanh Huynh <hu.thanh89@yahoo.com>
2018-09-24 01:50:33 -0700

    tune jumbotron hr

--- src/profile/component/about.js
+++ src/profile/component/about.js
@@ -14,7 +14,7 @@ class Component extends React.Component {
       <div class="jumbotron">
         <h1 class="display-4">Hi there!</h1>
         <p class="lead">I'm a software engineer with 4 years experience with the latest technogies. Looking to be hired as a full stack javascript engineer.</p>
-        <hr class="my-4"/>
+        <hr class="my-4 bg-light"/>
         <p>Feel free to give me a call or send me an email.</p>
         <p class="lead">
           <a class="btn btn-primary btn-lg" href="#" role="button" data-toggle="modal" data-target="#contactModal">
