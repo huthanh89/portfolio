45b29a711d574627b1a5b3f422f7992c0f64b4c7
Thanh Huynh <hu.thanh89@yahoo.com>
2018-09-26 18:28:27 -0700

    style card header

--- src/js/home/component/about.js
+++ src/js/home/component/about.js
@@ -12,8 +12,8 @@ class Component extends React.Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1 className="display-4">Hi there!</h1>
-        <p className="lead">I'm a software engineer with years of experience developing web applications. Looking to be hired as a full stack javascript engineer.</p>
+        <h1 className="display-4">Hi, I'm Thanh!</h1>
+        <p className="lead">A software engineer with years of experience developing web applications. Looking to be hired as a full stack javascript engineer.</p>
         <hr className="my-4 bg-light"/>
         <p>Feel free to give me a call or send me an email.</p>
         <p className="lead">
