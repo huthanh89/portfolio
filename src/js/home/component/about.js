25b2091d8258cf21db8ab6aef4241f28f7f1a5e5
Thanh Huynh <hu.thanh89@yahoo.com>
2018-10-13 00:41:01 -0700

    Tune jumbo tron

--- src/js/home/component/about.js
+++ src/js/home/component/about.js
@@ -14,19 +14,22 @@ class Component extends React.Component {
       <div className="jumbotron">
         <div className="row">
           <div className="col-lg-12">
-            <h1 className="display-4">Hi, I'm Thanh!</h1>
+            <h1 className="display-5">Hi, I'm Thanh!</h1>
             <p className="lead">A software engineer with years of experience developing web applications. Looking to be hired as a full stack javascript engineer.</p>
             <hr className="my-4 bg-light"/>
-
-            <p>Feel free to give me a call or send me an email.</p>
-
-            <button type="button" className="float-right btn btn-lg bg-primary btn-outline-light" data-toggle="modal" data-target="#contactModal">
-              <i className="fas fa-user mr-2"></i>
-              <span>
-                Let's connect
-              </span> 
-            </button>
-
+            <div className="row">
+              <div className="col-lg-6">
+                <p>Feel free to give me a call or send me an email.</p>
+              </div>
+              <div className="col-lg-6">
+                <button type="button" className="float-right btn btn-lg bg-primary btn-outline-light" data-toggle="modal" data-target="#contactModal">
+                  <i className="fas fa-user mr-2"></i>
+                  <span>
+                    Let's connect
+                  </span> 
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
