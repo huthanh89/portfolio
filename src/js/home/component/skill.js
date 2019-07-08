8ec23a02e0388ca1eaca28eb3867fba68b7153e5
Thanh Huynh <hu.thanh89@yahoo.com>
2019-07-08 00:41:59 -0700

    Add to skills

--- src/js/home/component/skill.js
+++ src/js/home/component/skill.js
@@ -9,48 +9,48 @@ import React from 'react';
 //-----------------------------------------------------------------------------//
 
 class Component extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="card">
-          <div className="card-header">
-            <i className="fas fa-laptop fa-fw"/>
-              <b>
-                &nbsp;Computer Skills
-              </b>
+    render() {
+        return (
+        <div>
+            <div className="card">
+            <div className="card-header">
+                <i className="fas fa-laptop fa-fw"/>
+                <b>
+                    &nbsp;Computer Skills
+                </b>
+                </div>
+            <div className="card-body">
+                <ul className="card-text">
+                <li>
+                    <b>
+                        Languages:&nbsp;
+                    </b>
+                    <span>
+                        Batch Script, Unix/Shell Scripting, Python, CSS, HTML, JavaScript
+                    </span>
+                </li>
+                <li>
+                    <b>
+                        Back End Technologies:&nbsp;
+                    </b>
+                    <span>
+                        Aws, Nginx, MongoDB, Express, NodeJS
+                    </span>
+                </li>
+                <li>
+                    <b>
+                        Front End Technologies:&nbsp;
+                    </b>
+                    <span>
+                        Backbone, Marionette, JQuery, JSON, Highcharts, D3, Bootstrap, Gulp, Webpack, Jenkins, Git, Angular, Redux, React
+                    </span>
+                </li>
+                </ul>
             </div>
-          <div className="card-body">
-            <ul className="card-text">
-              <li>
-                <b>
-                  Languages:&nbsp;
-                </b>
-                <span>
-                  Python, CSS, HTML, JavaScript
-                </span>
-              </li>
-              <li>
-                <b>
-                  Back End Technologies:&nbsp;
-                </b>
-                <span>
-                  Aws, Nginx, MongoDB, Express, Node
-                </span>
-              </li>
-              <li>
-                <b>
-                  Front End Technologies:&nbsp;
-                </b>
-                <span>
-                  Backbone, Marionette, JQuery, JSON, Highcharts, D3, Bootstrap, Gulp, Webpack, Git, Redux, React
-                </span>
-              </li>
-            </ul>
-          </div>
+            </div>
         </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 //-----------------------------------------------------------------------------//
